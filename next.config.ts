633572ad9ee813df9a1ee2d19da6b83a5d5904fa
nillathiya <73015219+nillathiya@users.noreply.github.com>
2025-05-19 19:01:19 +0530

    yers

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "localhost", "192.168.29.103"],
+    domains: ["lh3.googleusercontent.com", "localhost", "192.168.29.103","192.168.29.163"],
   },
   // reactStrictMode: false,
 };
